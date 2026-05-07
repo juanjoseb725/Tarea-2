7d9fc4728068ab2ac616adc8bbff81ec07497abd
juanjoseb725 <juanjoseb725@gmail.com>
2026-05-06 23:31:28 -0600

    Add program to calculate area and circumference

---
+++ 2Simple.c
@@ -0,0 +1,17 @@
+//Programa de radio y circunferencia
+#include<stdio.h>
+
+void main()
+{
+float radio ;
+float pi=3.141592 ;
+float circunferencia, area;
+printf("Bienvenido este programa calculara Area y Radio\n");
+printf("Ingrese el radio\n");
+scanf( "%f", &radio);
+area=pi*radio*radio;
+circunferencia=2*pi*radio;
+printf("Area: %f\n", area);
+printf("Circunferencia: %f\n", circunferencia); 
+
+}
