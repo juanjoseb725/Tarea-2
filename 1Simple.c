d2a45595e99a47d9d928584e568a7ed4c97b2403
juanjoseb725 <juanjoseb725@gmail.com>
2026-05-06 23:10:33 -0600

    Create 1Simple.c

---
+++ 1Simple.c
@@ -0,0 +1,20 @@
+//Bautista Valdivia Juan Jose, Programa que suma , multiplica y resta dos numeros
+
+#include <stdio.h>
+
+void main()
+{
+int a, b;
+int suma, resta, multiplicacion;
+printf("Bienvenido este programa te dara resultado de suma resta y multiplicacion\n");
+printf("Ingrese los dos numeros a sumar separados por una coma\n");
+scanf("%d , %d", &a, &b);
+suma=a+b;
+resta=a-b;
+multiplicacion=a*b;
+printf("Resultados\n");
+printf("Suma: %d\n", suma);
+printf("Resta: %d\n", resta);
+printf("Multiplicacion: %d\n", multiplicacion);
+
+}
