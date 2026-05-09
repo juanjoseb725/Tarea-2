7029a6a1cc14ab4413666793b148c4a433569cea
juanjoseb725 <juanjoseb725@gmail.com>
2026-05-08 21:34:44 -0600

    Reformat code for area and circumference calculation

--- 2Simple.c
+++ 2Simple.c
@@ -3,15 +3,15 @@
 
 void main()
 {
-float radio ;
-float pi=3.141592 ;
-float circunferencia, area;
-printf("Bienvenido este programa calculara Area y Radio\n");
-printf("Ingrese el radio\n");
-scanf( "%f", &radio);
-area=pi*radio*radio;
-circunferencia=2*pi*radio;
-printf("Area: %f\n", area);
-printf("Circunferencia: %f\n", circunferencia); 
+  float radio ;
+  float pi=3.141592 ;
+  float circunferencia, area;
+  printf("Bienvenido este programa calculara Area y Radio\n");
+  printf("Ingrese el radio\n");
+  scanf( "%f", &radio);
+  area=pi*radio*radio;
+  circunferencia=2*pi*radio;
+  printf("Area: %f\n", area);
+  printf("Circunferencia: %f\n", circunferencia); 
 
 }
