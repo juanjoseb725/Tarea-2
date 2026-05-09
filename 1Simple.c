21cde16cdf8daf8461bfbb87dda56b3468cadc7c
juanjoseb725 <juanjoseb725@gmail.com>
2026-05-08 21:34:23 -0600

    Format code for better readability

--- 1Simple.c
+++ 1Simple.c
@@ -4,17 +4,17 @@
 
 void main()
 {
-int a, b;
-int suma, resta, multiplicacion;
-printf("Bienvenido este programa te dara resultado de suma resta y multiplicacion\n");
-printf("Ingrese los dos numeros a sumar separados por una coma\n");
-scanf("%d , %d", &a, &b);
-suma=a+b;
-resta=a-b;
-multiplicacion=a*b;
-printf("Resultados\n");
-printf("Suma: %d\n", suma);
-printf("Resta: %d\n", resta);
-printf("Multiplicacion: %d\n", multiplicacion);
+  int a, b;
+  int suma, resta, multiplicacion;
+  printf("Bienvenido este programa te dara resultado de suma resta y multiplicacion\n");
+  printf("Ingrese los dos numeros a sumar separados por una coma\n");
+  scanf("%d , %d", &a, &b);
+  suma=a+b;
+  resta=a-b;
+  multiplicacion=a*b;
+  printf("Resultados\n");
+  printf("Suma: %d\n", suma);
+  printf("Resta: %d\n", resta);
+  printf("Multiplicacion: %d\n", multiplicacion);
 
 }
