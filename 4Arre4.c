b1c3ee1c4a86d4706583bbdd27caa6216a3e5736
juanjoseb725 <juanjoseb725@gmail.com>
2026-05-14 23:31:15 -0600

    Create 4Arre4.c

---
+++ 4Arre4.c
@@ -0,0 +1,22 @@
+//Construir un programa para almacenar en un arreglo unidimensional los 100 primeros n ́umeros pares. Imprimir al nal el arreglo.
+#include<stdio.h>
+
+void main()
+{
+int i;
+int enteros[100];
+printf("Bienvenido en este programa guardaremos los 100 primeros numeros pares\n");
+
+
+for(i=0; i<100;i++)
+ {
+ enteros[i]=2*i;
+ printf("%d\n",enteros[i]);
+ 
+ 
+ }
+
+  
+  
+  
+}
